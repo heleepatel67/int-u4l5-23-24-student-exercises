3481f767cf870e9baf32638b7aeb6c610f1f4f0e
heleepatel67 <heleepatel67@gmail.com>
2024-06-17 15:47:04 -0400

    4.5

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -2,8 +2,8 @@
 
 // 1. Create a secret username and password.
 //  - The variables are declared already. Update the values.
-let secretUsername = ("");
-let secretPassword = ("");
+let secretUsername = ("hkpatel");
+let secretPassword = ("1234");
 let button = document.querySelector("button");
 
 button.addEventListener("click", function() {
@@ -13,7 +13,14 @@ button.addEventListener("click", function() {
 
     // 2. Create a compound conditional statement to check for the following:
     //  - If the username AND password is correct, update the text of the message div to say "Success!"
-    
+    if (username === secretUsername && password === secretPassword) {
+        message.innerHTML = "yay!";
+    } else if (username === secretUsername && password !== secretPassword){
+        message.innerHTML = "Wrong password - be careful, don't mess this up too many times"
+    } else if (username !== secretUsername && password === secretPassword) {
+        message.innerHTML = "Wrong username - is there a typo?"
+    } else if (username !== secretUsername && password !== secretPassword )
+        message.innerHTML = "Incorrect username and password. Hmm. Suspicious."
     // CODE SOLO
     // 3. Create additional compound conditionals that check for the following:
     //  - If the username is correct AND the password is incorrect, update the text of the message div to say "Wrong password - be careful, don't mess this up too many times."
--- script.js
+++ script.js
@@ -9,10 +9,16 @@ button.addEventListener("click", function() {
     // 1. Add an else-if and else so that:
     //  - If the water temp is greater than 32°, the user knows it's ok to drink.
     //  - If the water temp is 32° or less, the user knows the water is frozen.
+
+    if (waterTemp < 32 || waterTemp > 212) {
+        alert("Don't drink it!");
+      }  else {
+            alert("water is safe!");
+        }
+    }
     
     
-    
-});
+);
 
 // Bonus: According to research, 155°F is an optimal temperature for consuming hot drinks. 
 //  - How would you improve the conditional statements above so that your code gives some more helpful information to the user?
